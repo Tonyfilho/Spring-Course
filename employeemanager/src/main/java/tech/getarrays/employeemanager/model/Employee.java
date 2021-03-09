3e5208de40ffa47616b4608bd99fc140e2f8fae6
tony filho <antonio.m.filho>
2021-03-09 18:03:05 +0000

    1º entity

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,31 @@
+package tech.getarrays.employeemanager.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee implements Serializable{
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+
+        public Employee(){};
+        
+
+
+    
+}// end class
