226be6a19b7484c9330bde0f6015b16f8cda0538
tony filho <antonio.m.filho>
2021-03-21 15:13:50 +0000

    service employee is done

--- employeemanager/employeemanagerweb/src/app/employee/employee-service.service.ts
+++ employeemanager/employeemanagerweb/src/app/employee/employee-service.service.ts
@@ -1,3 +1,4 @@
+import { EmployeeInterface } from './../../assets/common/employee';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -9,9 +10,25 @@ export class EmployeeServiceService {
  private apiServerUrl = '';
   constructor(private http: HttpClient) { };
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/`);
+  public getEmployees(): Observable<EmployeeInterface[]> {
+    return this.http.get<EmployeeInterface[]>(`${this.apiServerUrl}/employee/`);
   }
+
+  public addEmployee(employee: EmployeeInterface): Observable<EmployeeInterface>{
+    return this.http.post<EmployeeInterface>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: EmployeeInterface): Observable<EmployeeInterface>{
+    return this.http.put<EmployeeInterface>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number):Observable<void>{
+   return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+
+  
+
+
 
 
 
